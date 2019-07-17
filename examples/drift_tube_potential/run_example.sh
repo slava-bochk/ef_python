2020f13807bc4260f39d7d734129ed1da277a1c2
Ivan Kadochnikov <kadivas@jinr.ru>
2019-07-17 17:29:51 +0300

    Rewrite jupyter example testing and test drift tube potential notebook

--- examples/drift_tube_potential/run_example.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ef pot.conf
-python3 plot.py
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 from os.path import basename
-from shutil import copyfile
+from shutil import copytree, copy
 
 import pytest
 
@@ -14,27 +14,20 @@ _examples_conf = [("examples/minimal_working_example/minimal_conf.conf", ()),
                    pytest.mark.slowish),
                   ("examples/single_particle_in_magnetic_field/large_time_step.conf",
                    pytest.mark.slowish),
-                  ("examples/tube_source_test/contour.conf",
-                   pytest.mark.slow),
                   ("examples/single_particle_in_radial_electric_field/single_particle_in_radial_electric_field.conf",
                    pytest.mark.slowish),
-                  ("examples/ribbon_beam_contour/contour_bin.conf",
+                  ("examples/tube_source_test/contour.conf",
                    pytest.mark.slow),
+                  ("examples/ribbon_beam_contour/contour_bin.conf", ()),
                   ("examples/ribbon_beam_contour/contour.conf",
-                   pytest.mark.slow),
-                  ("examples/drift_tube_potential/pot.conf",
-                   pytest.mark.slow)]
-
-_examples_jupyter = [("examples/axially_symmetric_beam_contour/axially_symmetric_beam_contour.ipynb",
-                      pytest.mark.slow)]
+                   pytest.mark.slowish)]
 
 _pytest_params_example_conf = [pytest.param(f.replace('/', os.path.sep), marks=m) for f, m in _examples_conf]
-_pytest_params_example_jupyter = [pytest.param(f.replace('/', os.path.sep), marks=m) for f, m in _examples_jupyter]
 
 
 @pytest.mark.parametrize("fname", _pytest_params_example_conf)
 def test_example_conf(fname, mocker, capsys, tmpdir, monkeypatch):
-    copyfile(fname, tmpdir.join(basename(fname)))
+    copy(fname, tmpdir.join(basename(fname)))
     monkeypatch.chdir(tmpdir)
     mocker.patch("sys.argv", ["main.py", str(basename(fname))])
     main()
@@ -42,24 +35,31 @@ def test_example_conf(fname, mocker, capsys, tmpdir, monkeypatch):
     assert err == ""
 
 
+def run_jupyter(dir, fname, path=None, copy_dir=False):
+    dir = dir.replace('/', os.path.sep)
+    fname = os.path.join(dir, fname)
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor
+    with open(fname) as f:
+        nb = nbformat.read(f, as_version=4)
+    if copy_dir:
+        copytree(dir, path)
+    ep = ExecutePreprocessor(timeout=600)
+    ep.preprocess(nb, {'metadata': {'path': path}} if path is not None else {})
+
+
 def test_all_examples_visualize():
-    import nbformat
-    from nbconvert.preprocessors import ExecutePreprocessor
-    fname = "examples/jupyter/visualize_examples.ipynb".replace('/', os.path.sep)
-    with open(fname) as f:
-        nb = nbformat.read(f, as_version=4)
-    ep = ExecutePreprocessor(timeout=600)
-    ep.preprocess(nb, {'metadata': {'path': 'examples/jupyter/'}})
+    run_jupyter("examples/jupyter", "visualize_examples.ipynb", 'examples/jupyter/')
 
 
-@pytest.mark.parametrize("fname", _pytest_params_example_jupyter)
-def test_example_jupyter(fname, tmpdir):
-    import nbformat
-    from nbconvert.preprocessors import ExecutePreprocessor
-    with open(fname) as f:
-        nb = nbformat.read(f, as_version=4)
-    ep = ExecutePreprocessor(timeout=600)
-    ep.preprocess(nb, {'metadata': {'path': tmpdir}})
+@pytest.mark.slow
+def test_axially_symmetric_beam_contour(tmpdir):
+    run_jupyter("examples/axially_symmetric_beam_contour", "axially_symmetric_beam_contour.ipynb", tmpdir)
+
+
+@pytest.mark.slowish
+def test_drift_tube_potential(tmpdir):
+    run_jupyter("examples/drift_tube_potential", "potential.ipynb", tmpdir.join('newdir'), True)
 
 
 @pytest.mark.requires_install
