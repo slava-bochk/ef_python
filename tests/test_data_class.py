4e2955cc5ad89a5828cab45db81adfa090c0e94b
Ivan Kadochnikov <kadivas@jinr.ru>
2019-08-09 17:17:08 +0300

    Rewrite DataClass for more flexibility, test more

--- tests/test_data_class.py
+++ tests/test_data_class.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pytest
+from pytest import raises
 
-from ef.util.data_class import DataClass, DataClassHashable
+from ef.util.data_class import DataClass
 
 
 class TestDataClass:
@@ -42,7 +42,6 @@ class TestDataClass:
         ab = AB(1, 2)
         assert ab.dict == {'a': 1, 'b': 2}
         assert AB(ab, []).dict == {'a': ab, 'b': []}
-        assert AB(**ab.dict) == ab
         assert self.ABx(1, 2).dict == {'a': 1, 'b': 2, 'x': 3}
         assert self.AB_x(1, 2).dict == {'a': 1, 'b': 2}
 
@@ -52,39 +51,56 @@ class TestDataClass:
         assert ab == ab
         assert ab == AB(1, 2)
         assert ab != AB(2, 2)
-        assert ab == AB(1., 2)
         assert ab != (1, 2)
         assert ab != self.AB2(1, 2)
-        assert ab != self.ABx(1, 2)
-        assert ab != self.AB_x(1, 2)
-        assert AB([1, 2, 3], np.array([[4, 5, 6], [7, 8, 9]])) == \
-               AB([1, 2, 3], np.array([[4, 5, 6], [7, 8, 9]]))
-        assert AB([1, 2, 3], np.array([[4, 5, 6], [7, 8, 9]])) != \
-               AB([1, 2, 3], np.array([[4, 5, 7], [7, 8, 9]]))
-        assert AB([1, 2, 3], np.array([[4, 5, 6], [7, 8, 9]])) == \
-               AB([1, 2, 3], np.array([[4, 5, 6.], [7, 8, 9]]))
-        assert AB(AB(1, 2), 5) == AB(AB(1, 2), 5)
-        assert AB(AB(1, 3), 5) != AB(AB(1, 2), 5)
-        assert eval(repr(ab)) == ab
-        ab.x = 10
-        assert ab != AB(1, 2)
 
-    def test_hash(self):
-        class ABh(self.AB, DataClassHashable):
-            def __hash__(self):
-                return super().__hash__()
+    def test_assert_eq(self):
+        AB = self.AB
+        ab = AB(1, 2)
+        ab.assert_eq(ab)
+        ab.assert_eq(AB(1, 2))
+        with raises(AssertionError, match='a'):
+            ab.assert_eq(AB(2, 2))
+        with raises(AssertionError, match=''):
+            ab.assert_eq((1, 2))
+        with raises(AssertionError, match='xyz'):
+            ab.assert_eq(self.AB2(1, 2), 'xyz')
 
-        x = ABh(1, 2)
-        xf = ABh(1., 2.)
-        y = ABh(2, 3)
-        good = ABh((), ())
-        bad = ABh([], [])
-        assert len({x, xf}) == 1
-        assert len({x, y}) == 2
-        assert len({x, y, xf}) == 2
-        assert len({good, good, x}) == 2
-        with pytest.raises(TypeError):
-            y = {bad, x}
+    def test_attr_eq(self):
+        AB = self.AB
+        eq = DataClass._attr_eq
+        assert eq(1, 1.)
+        assert eq([1, 2, 3], [1, 2, 3])
+        assert eq(np.array([[4, 5, 6], [7, 8, 9]]), np.array([[4, 5, 6], [7, 8, 9]]))
+        assert not eq(np.array([[4, 5, 6], [7, 8, 9]]), np.array([[4, 5, 7], [7, 8, 9]]))
+        assert eq(np.array([4, 5, 6]), np.array([4, 5, 6.]))
+        assert eq(AB(AB(1, 2), 5), AB(AB(1, 2), 5))
+        assert not eq(AB(AB(1, 3), 5), AB(AB(1, 2), 5))
+
+    def test_assert_attr_eq(self):
+        AB = self.AB
+        _assert_eq = DataClass._assert_attr_eq
+        _assert_eq(1, 1.)
+        _assert_eq([1, 2, 3], [1, 2, 3])
+        _assert_eq(np.array([[4, 5, 6], [7, 8, 9]]), np.array([[4, 5, 6], [7, 8, 9]]))
+        with raises(AssertionError, match='path'):
+            _assert_eq(np.array([[4, 5, 6], [7, 8, 9]]), np.array([[4, 5, 7], [7, 8, 9]]), 'path')
+        with raises(AssertionError, match=''):
+            _assert_eq(np.array([[4, 5, 6], [7, 8, 9]]), np.array([[4, 5, 7], [7, 8, 9]]))
+        _assert_eq(np.array([4, 5, 6]), np.array([4, 5, 6.]))
+        _assert_eq(AB(AB(1, 2), 5), AB(AB(1, 2), 5))
+        with raises(AssertionError, match=r'a\.b'):
+            _assert_eq(AB(AB(1, 3), 5), AB(AB(1, 2), 5))
+        with raises(AssertionError, match=r'root\.a\.b'):
+            _assert_eq(AB(AB(1, 3), 5), AB(AB(1, 2), 5), 'root')
+
+    def test_attr_eq_override(self):
+        class ABchild(self.AB):
+            @staticmethod
+            def _attr_eq(a, b):
+                return True
+
+        assert ABchild(1, 13) == ABchild(3.14, 'spam')
 
     def test_nan(self):
         assert self.AB([1, 2, 3], np.array([[4, 5, np.NaN], [7, 8, 9]])) != \
@@ -95,3 +111,12 @@ class TestDataClass:
 
     def test_repr(self):
         assert repr(self.AB(1, 2)) == "AB(a=1, b=2)"
+
+    def test_dict_init(self):
+        ab = self.AB(1, 'ham')
+        assert self.AB(**ab.dict_init) == ab
+
+    def test_repr_init(self):
+        AB = self.AB
+        ab = AB(2, 'spam')
+        assert eval(repr(ab)) == ab
