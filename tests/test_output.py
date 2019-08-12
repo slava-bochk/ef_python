e21b4f1e5bbbb9087a5e980d204b27641edf4551
Ivan Kadochnikov <kadivas@jinr.ru>
2019-08-12 14:45:54 +0300

    Use Simulation directly instead of Config

--- tests/test_output.py
+++ tests/test_output.py
@@ -3,44 +3,49 @@ import pytest
 
 from ef.config.components import *
 from ef.config.config import Config
+from ef.field.expression import FieldExpression
+from ef.field.uniform import FieldUniform
+from ef.inner_region import InnerRegion
+from ef.meshgrid import MeshGrid
 from ef.output import OutputWriterNumberedH5, OutputWriterNone
 from ef.output.cpp import OutputWriterCpp
 from ef.output.history import OutputWriterHistory
 from ef.output.python import OutputWriterPython
 from ef.output.reader import Reader
+from ef.particle_interaction_model import Model
+from ef.particle_source import ParticleSource
+from ef.simulation import Simulation
+from ef.time_grid import TimeGrid
 
 
 @pytest.fixture()
-def sim_full_config():
-    return Config(TimeGridConf(200, 20, 2), SpatialMeshConf((5, 5, 5), (.1, .1, .1)),
-                  sources=[ParticleSourceConf('a', Box()),
-                           ParticleSourceConf('c', Cylinder()),
-                           ParticleSourceConf('d', Tube(start=(0, 0, 0), end=(0, 0, 1)))],
-                  inner_regions=[InnerRegionConf('1', Box(), 1),
-                                 InnerRegionConf('2', Sphere(), -2),
-                                 InnerRegionConf('3', Cylinder(), 0),
-                                 InnerRegionConf('4', Tube(), 4)],
-                  output_file=OutputFileConf(), boundary_conditions=BoundaryConditionsConf(-2.7),
-                  particle_interaction_model=ParticleInteractionModelConf('binary'),
-                  external_fields=[ExternalElectricFieldUniformConf('x', (-2, -2, 1)),
-                                   ExternalMagneticFieldExpressionConf('y',
-                                                                       ('0', '0',
-                                                                        '3*x + sqrt(y) - z**2'))])
+def sim_full():
+    return Simulation(TimeGrid(200, 2, 20), MeshGrid(5, 51),
+               particle_sources=[ParticleSource('a', Box()),
+                                 ParticleSource('c', Cylinder()),
+                                 ParticleSource('d', Tube(start=(0, 0, 0), end=(0, 0, 1)))],
+               inner_regions=[InnerRegion('1', Box(), 1),
+                              InnerRegion('2', Sphere(), -2),
+                              InnerRegion('3', Cylinder(), 0),
+                              InnerRegion('4', Tube(), 4)],
+               particle_interaction_model=Model.binary,
+               electric_fields=[FieldUniform('x', 'electric', (-2, -2, 1))],
+               magnetic_fields=[FieldExpression('y', 'magnetic', '0', '0', '3*x + sqrt(y) - z**2')])
 
 
 @pytest.mark.usefixtures('backend')
 class TestOutput:
-    def test_write_none(self, monkeypatch, tmpdir, sim_full_config):
+    def test_write_none(self, monkeypatch, tmpdir, sim_full):
         monkeypatch.chdir(tmpdir)
-        sim = sim_full_config.make()
+        sim = sim_full
         writer = OutputWriterNone()
         writer.write(sim, 'asdf')
         writer.write(sim)
         assert tmpdir.listdir() == []
 
-    def test_write_cpp(self, monkeypatch, tmpdir, sim_full_config):
+    def test_write_cpp(self, monkeypatch, tmpdir, sim_full):
         monkeypatch.chdir(tmpdir)
-        sim = sim_full_config.make()
+        sim = sim_full
         writer = OutputWriterCpp('test_', '.ext')
         writer.write(sim, 'asdf')
         assert tmpdir.join('test_asdf.ext').exists()
@@ -50,9 +55,9 @@ class TestOutput:
             assert Reader().guess_h5_format(h5file) == 'cpp'
             Reader().read_simulation(h5file).assert_eq(sim)
 
-    def test_write_python(self, monkeypatch, tmpdir, sim_full_config):
+    def test_write_python(self, monkeypatch, tmpdir, sim_full):
         monkeypatch.chdir(tmpdir)
-        sim = sim_full_config.make()
+        sim = sim_full
         writer = OutputWriterPython('test_', '.ext')
         writer.write(sim, 'asdf')
         assert tmpdir.join('test_asdf.ext').exists()
@@ -62,9 +67,9 @@ class TestOutput:
             assert Reader().guess_h5_format(h5file) == 'python'
             Reader().read_simulation(h5file).assert_eq(sim)
 
-    def test_write_history(self, monkeypatch, tmpdir, sim_full_config):
+    def test_write_history(self, monkeypatch, tmpdir, sim_full):
         monkeypatch.chdir(tmpdir)
-        sim = sim_full_config.make()
+        sim = sim_full
         assert tmpdir.listdir() == []
         writer = OutputWriterHistory('test_', '.ext')
         assert tmpdir.join('test_history.ext').exists()
