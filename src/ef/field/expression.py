5db47a5e0d8c5814e9bf6a2a55e9e02d2b749fa5
Ivan Kadochnikov <kadivas@jinr.ru>
2019-08-14 12:23:54 +0300

    Cupy expresion field

--- src/ef/field/expression.py
+++ src/ef/field/expression.py
@@ -1,37 +1,52 @@
-import math
+import ast
+import operator as op
 
-import numpy as np
+import inject
+import numpy
+from simpleeval import SimpleEval
 
 from ef.field import Field
-from simpleeval import SimpleEval
+from ef.util.inject import safe_default_inject
 
 
 class FieldExpression(Field):
 
-    def __init__(self, name, electric_or_magnetic, expression_x, expression_y, expression_z):
+    @safe_default_inject
+    @inject.params(xp=numpy)
+    def __init__(self, name, electric_or_magnetic, expression_x, expression_y, expression_z, xp=numpy):
         super().__init__(name, electric_or_magnetic)
+        self._xp = xp
         self.expression_x = expression_x
         self.expression_y = expression_y
         self.expression_z = expression_z
-        self._ev = SimpleEval(functions={"sin": math.sin,
-                                         "cos": math.cos,
-                                         "sqrt": math.sqrt})
+        self._ev = SimpleEval(functions={"sin": xp.sin,
+                                         "cos": xp.cos,
+                                         "sqrt": xp.sqrt},
+                              operators={ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+                                         ast.Div: op.truediv, ast.FloorDiv: op.floordiv,
+                                         ast.Pow: op.pow, ast.Mod: op.mod,
+                                         ast.Eq: op.eq, ast.NotEq: op.ne,
+                                         ast.Gt: op.gt, ast.Lt: op.lt,
+                                         ast.GtE: op.ge, ast.LtE: op.le,
+                                         ast.Not: op.not_,
+                                         ast.USub: op.neg, ast.UAdd: op.pos,
+                                         ast.In: lambda x, y: op.contains(y, x),
+                                         ast.NotIn: lambda x, y: not op.contains(y, x),
+                                         ast.Is: lambda x, y: x is y,
+                                         ast.IsNot: lambda x, y: x is not y,
+                                         }
+                              )
         # todo: inherit SimpleEval and define math functions inside
         # todo: add r, theta, phi names
 
-    def _field_at_position(self, pos):
-        self._ev.names["x"] = pos[0]
-        self._ev.names["y"] = pos[1]
-        self._ev.names["z"] = pos[2]
-        fx = self._ev.eval(self.expression_x)
-        fy = self._ev.eval(self.expression_y)
-        fz = self._ev.eval(self.expression_z)
-        return fx, fy, fz
-
-    def get_at_points(self, positions, time):
-        positions = np.asarray(positions)
+    def get_at_points(self, positions, time: float) -> numpy.ndarray:
+        positions = self._xp.asarray(positions)
         self._ev.names["t"] = time
-        if positions.shape == (3,):
-            return np.array(self._field_at_position(positions))
-        else:
-            return np.array([self._field_at_position(pos) for pos in positions])
+        self._ev.names["x"] = positions[:, 0]
+        self._ev.names["y"] = positions[:, 1]
+        self._ev.names["z"] = positions[:, 2]
+        result = self._xp.empty_like(positions)
+        result[:, 0] = self._ev.eval(self.expression_x)
+        result[:, 1] = self._ev.eval(self.expression_y)
+        result[:, 2] = self._ev.eval(self.expression_z)
+        return result
