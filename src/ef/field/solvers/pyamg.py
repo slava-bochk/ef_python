4a99faf266008c19af13a63c802e8ade2f546da9
Ivan Kadochnikov <kadivas@jinr.ru>
2020-02-14 13:04:15 +0300

    Use universal amg solver

--- src/ef/field/solvers/pyamg.py
+++ src/ef/field/solvers/pyamg.py
@@ -5,9 +5,9 @@ class FieldSolverPyamg(FieldSolver):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         import pyamg
-        self._solver = pyamg.ruge_stuben_solver(self.A)
+        self._solver = pyamg
 
     def eval_potential(self, charge_density, potential):
         self.init_rhs_vector(charge_density, potential)
-        self.phi_vec = self._solver.solve(self.rhs, x0=self.phi_vec, tol=self.tolerance, maxiter=self.max_iter)
+        self.phi_vec = self._solver.solve(self.A, self.rhs, x0=self.phi_vec, tol=self.tolerance, verb=False, maxiter=self.max_iter)
         self.transfer_solution_to_spat_mesh(potential)
